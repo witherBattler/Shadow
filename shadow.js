33429630a113ace250084145c163fb9705d97249
IMPixel <asemenov.ca@gmail.com>
2022-02-19 10:35:42 -0500

    ?

--- shadow.js
+++ shadow.js
@@ -35,56 +35,7 @@ function windowResized() {
         resizeCanvas(windowWidth, windowHeight);
     }
 }
-function drawStage(stage) {
-    var objectsToDraw = []
-    for(var i = 0; i != stage.groups.length; i++) {
-        for(var t; t != stage.groups[i].objects.length; t++) {
-            var workingObject = stage.groups[i].objects[t]
-            objectsToDraw.push(workingObject)
-        }
-    }
-    for(var i = 0; i != stage.objects.length; i++) {
-        objectsToDraw.push(stage.objects[i])
-    }
-    for(var i = 0; i != objectsToDraw.length; i++) {
-        drawObject(objectsToDraw[i])
-    }
-}
-function drawObject(object) {
-    if(object.visible){
-        switch(object.objectType) {
-            case "rect":
-                drawRect(object)
-                break;
-            default:
-                throw new Error("Wrong object type on object.")
-                break;
-        }
-        for(var i = 0; i != object.lightSources.length; i++) {
-            additionalLightSources.push(object.lightSources[i])
-            additionalLightSources[i].x = object.x
-            additionalLightSources[i].y = object.y
-        }
-    }
-    drawTextForObject(object)
-}
-function drawRect(object) {
-    stroke(object.stroke)
-    strokeWeight(object.strokeWeight)
-    fill(object.shapeColor)
-    textAlign(CENTER)
-    rectMode(CENTER)
-    rect(object.x, object.y, object.width * object.scale, object.height * object.scale)
-}
-function drawTextForObject(object) {
-    fill(object.textColor)
-    stroke(object.textStroke)
-    strokeWeight(object.textStrokeWeight)
-    textSize(object.textSize)
-    textFont(object.textFont)
-    textAlign(CENTER, CENTER)
-    text(object.text, object.x + object.textOffsetX, object.y + object.textOffsetY)
-}
+
 function updateShadows(stage) {
     shadowOverlay.clear();
     // Black, covering entire screen, foreground
